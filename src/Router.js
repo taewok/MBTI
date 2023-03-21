291bf5f5bf489d7fc6b13f568a901964ba619bcd
taewok <taewok5163@naver.com>
2023-03-21 20:26:22 +0900

    feat: 질의응답 페이지 route 연결

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./page/Home";
+import Question from "./page/Question";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/question/*" element={<Question />} />
     </Routes>
   );
 };
