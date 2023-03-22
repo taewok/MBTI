8e8b4fc9411c08913b9fd0daf4e2492cf93c9183
taewok <taewok5163@naver.com>
2023-03-22 11:54:27 +0900

    feat: 질문 list 변수 questionList 생성

--- src/page/Question.js
+++ src/page/Question.js
@@ -1,11 +1,114 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import styled from "styled-components";
 
 const Question = () => {
-    return (
-        <div>
-            Question
-        </div>
-    );
+  const navigate = useNavigate();
+
+  const [num, setNum] = useState(1);
+
+  useEffect(() => {
+    navigate(`/question/${num}`);
+  }, [navigate, num]);
+
+  const questionList = [
+    {
+      question: "데이트가 없는 주말에 나는",
+      Q1: "침대랑 하루 종일 물아일체가 된다",
+      Q2: "단톡에 연락해서 친구들과 약속을 잡는다",
+      Q1Value: "E",
+      Q2Value: "I",
+    },
+    {
+      question: "친구의 소개로 소개팅에 나온 나는",
+      Q1: "먼저 말 걸면서 분위기를 띄운다",
+      Q2: "말을 걸어올 때까지 기다리고 본다",
+      Q1Value: "E",
+      Q2Value: "I",
+    },
+    {
+      question: "데이트 중 길에서 연인의 친구를 만난다면 나는",
+      Q1: "자연스럽게 웃으며 대한다 ",
+      Q2: "무생물이 되어 조용히 있는다",
+      Q1Value: "E",
+      Q2Value: "I",
+    },
+    {
+      question: "데이트 중 맛있어 보이는 밥집을 발견한 나는",
+      Q1: "간판에서 맛집의 기운이 느껴진다 맛집 각이야",
+      Q2: "유명하고 리뷰도 많으니까 맛은 보장되어 있겠군",
+      Q1Value: "S",
+      Q2Value: "N",
+    },
+    {
+      question: "오늘 본 영화를 궁금해하는 연인에게 나는",
+      Q1: "주인공이 좀비 바이러스가 퍼져서 치료하기 위한 영화야",
+      Q2: "좀비랑 싸우는데 주인공이 완전 멋져 보는 내내 소름 돋았어",
+      Q1Value: "S",
+      Q2Value: "N",
+    },
+    {
+      question: "연인에게 줄 선물을 고르게 된 나는",
+      Q1: "실용성은 없어도 예쁘고 기억에 남을 선물",
+      Q2: "연인에게 요즘 가장 필요할 것 같은 선물",
+      Q1Value: "S",
+      Q2Value: "N",
+    },
+    {
+      question: "연인과 사소한 일로 다퉜을 때 나는",
+      Q1: "나!! 진짜!! 너무!!! 화났어!!!!!",
+      Q2: "~점은 꼭 고쳐줬으면 좋겠어 이렇게 하면 되잖아",
+      Q1Value: "F",
+      Q2Value: "T",
+    },
+    {
+      question: "축 처진 연인이 우울하다고 말했을 때 나는",
+      Q1: "왜 우울해? 뭐 때문에 우울한 거야?",
+      Q2: "5초 만에 감정이입 완료. 같이 글썽거린다",
+      Q1Value: "F",
+      Q2Value: "T",
+    },
+    {
+      question: "힘들게 이벤트를 준비한 나를 신나게 할 연인의 칭찬은?",
+      Q1: "사랑해 최고야 나 완전 감동했어",
+      Q2: "고마워 요즘 바쁠 텐데 언제 이런 걸 생각했어?",
+      Q1Value: "F",
+      Q2Value: "T",
+    },
+    {
+      question: "데이트 룩을 고를 때 나는",
+      Q1: "전날부터 머리부터 발끝까지 세팅해 준다",
+      Q2: "나가기 직전 마음에 드는 옷을 입는다",
+      Q1Value: "J",
+      Q2Value: "P",
+    },
+    {
+      question: "썸 중에 연인이 집에 놀러 온다고 했을 때 나는",
+      Q1: "보이는 곳만 일단 급하게 치워둔다",
+      Q2: "쓰레기 버리기부터 화장실 청소까지 한다",
+      Q1Value: "J",
+      Q2Value: "P",
+    },
+    {
+      question: "커플 해외여행 계획을 짜게 된 나는",
+      Q1: "할 거면 제대로! 일별로 세부 일정을 정리한다",
+      Q2: "비행기 표만 끊어두고 계획의 80% 끝난다고 생각한다",
+      Q1Value: "J",
+      Q2Value: "P",
+    },
+  ];
+
+  return (
+    <Container>
+      <Section>
+        <QuestionText>Q, {questionList[num].question}</QuestionText>
+      </Section>
+    </Container>
+  );
 };
+
+const Container = styled.div``;
+const Section = styled.section``;
+const QuestionText = styled.div``;
 
 export default Question;
