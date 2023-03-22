765c67313603c517ca22ebe082504ecfc56cc9cf
taewok <taewok5163@naver.com>
2023-03-22 18:27:53 +0900

    feat: 결과화면 router 연결

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./page/Home";
 import Question from "./page/Question";
+import Result from "./page/Result";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/question/*" element={<Question />} />
+      <Route path="/question/result" element={<Result />} />
     </Routes>
   );
 };
